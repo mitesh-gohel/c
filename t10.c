5612307dc8398ea5b796ce0efc9be5b9b52458dc
mitesh-gohel <miteshgohel73@yahoo.com>
2020-04-19 12:37:08 +0530

    add t10.c(pow() function)

--- t10.c
+++ t10.c
@@ -9,9 +9,13 @@ int main()
 	int x=0, y=0, ans=0;
 	printf("For x^y enter x and y respectivly :");
 	scanf("%d%d",&x,&y);
-	
+	if(y<0)
+	{
+		printf("y should not be nagative \n");
+		return 0;
+	}	
 	ans = my_pow(x,y);
-	printf("x^y = %d",ans);
+	printf("x^y = %d \n",ans);
 	
 	return 0;
 }
@@ -19,6 +23,8 @@ int main()
 int my_pow(int x, int y)
 {
 	int ans = 1;
+	if(x==0)
+		return 0;
 	for(int i=0; i<y; ++i)
 	{
 		ans *= x;
